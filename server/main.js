44d25c55bb6096190479ed89cd8fd96e3f99713a
sleeper <shenyuan1996@126.com>
2017-12-26 22:50:41 +0800

    writing item query function.

--- server/main.js
+++ server/main.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var decode = require('./decode');
+var test = require('./test');
 var app = express();
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
+});
+app.get('/test',function(req, res){
+  var callback = test.testDecode("jd","4055764");
+  //console.log(callback);
+  res.send(callback);
 });
 app.get('/decode',function(req, res){
   var callback = decode.decode("jd","4055764");
